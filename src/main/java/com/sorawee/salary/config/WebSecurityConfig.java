dc53be91f5ca1b1739d6f6f74ad2a5c116211131
sorawee.p <47082062+sorawee-p@users.noreply.github.com>
2024-01-13 18:20:14 +0700

    Set up project - add log config - swagger-ui - security config - other dependencies

---
+++ src/main/java/com/sorawee/salary/config/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.sorawee.salary.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request.requestMatchers( "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**")
+                        .permitAll().anyRequest().authenticated());
+        return http.build();
+    }
+
+}
